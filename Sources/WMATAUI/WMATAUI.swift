264402587da4ca141dbb7dd0dda0ee16bf58b026
Randall Wood <297232+rhwood@users.noreply.github.com>
2022-01-09 10:28:26 -0500

    fix: set Image color using .foregroundColor property

--- Sources/WMATAUI/WMATAUI.swift
+++ Sources/WMATAUI/WMATAUI.swift
@@ -124,7 +124,7 @@ struct ContentView_Previews: PreviewProvider {
             Text("Metro Brand Icons:")
             HStack {
                 WMATAUI.roundel(image: Image(metroName: "train"), color: .gray, textColor: .purple, style: style)
-                WMATAUI.roundel(image: Image(metroName: "escalator.up"), color: .white, textColor: .green, style: style)
+                WMATAUI.roundel(image: Image(metroName: "accessible"), color: .white, textColor: .green, style: style)
                 WMATAUI.roundel(image: Image(metroName: "escalator.down"), color: .green, textColor: .white, style: style)
                 WMATAUI.roundel(image: Image(metroName: "bus"), color: .green, textColor: .black, style: style)
             }
